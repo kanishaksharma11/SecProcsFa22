289794eb4d823712760365ab1d5331a1e735706e
A59005455 <A59005455@eng.ucsd.edu>
2022-10-20 17:00:41 -0700

    Part2 1-bit transfer pretty much working

--- LAB1/Part2-DeadDrop/receiver.c
+++ LAB1/Part2-DeadDrop/receiver.c
@@ -6,7 +6,39 @@
 int main(int argc, char **argv)
 {
 	// Put your covert channel setup code here
+	
+	uint64_t *receiver_buffer = (uint64_t *)malloc(32768*sizeof(uint64_t));
+	if(NULL == receiver_buffer){
+		perror("Unable to malloc receiver buffer");
+		return EXIT_FAILURE;
+	}
 
+	volatile char tmp;
+	uint64_t l2_threshold;
+	uint64_t hit_time;
+	uint64_t l1_l3_delta = 7;
+	uint64_t hit_time_average = 0;
+	int reps = 10000;
+	int lines = 4096;
+
+	//prime L2
+	for(int i=0; i<lines; i++){
+		tmp = receiver_buffer[i*8];
+	}
+	//Calculate AMAT
+	for(int i=0; i<reps; i++){
+		hit_time = 0;
+		for(int i=0; i<lines; i++){
+			//tmp = receiver_buffer[i*8];
+			hit_time = hit_time + measure_one_block_access_time((uint64_t)(receiver_buffer+i*8));
+		}
+	hit_time_average += hit_time/lines;
+	}
+
+	hit_time_average = hit_time_average/reps;
+	l2_threshold = hit_time_average + l1_l3_delta;
+	printf("AMAT on a hit: %d\n", hit_time_average);
+	
 	printf("Please press enter.\n");
 
 	char text_buf[2];
@@ -16,14 +48,44 @@ int main(int argc, char **argv)
 
 	bool listening = true;
 	while (listening) {
-
+		
 		// Put your covert channel code here
-
+		uint64_t access_time = 0;
+		int hit = 0;
+		int miss = 0;
+		for(int i=0; i<lines; i=i+4){
+			access_time = measure_one_block_access_time((uint64_t)(receiver_buffer+8*i));
+			//printf("access_time = %d\t, threshold to cross for miss = %d\n", access_time, hit_time_average+l1_l3_delta);
+			if(access_time > hit_time_average+l1_l3_delta){
+				miss++;
+			} else {
+				hit ++;
+			}
+			access_time = measure_one_block_access_time((uint64_t)(receiver_buffer+8*(i+3)));
+			if(access_time > hit_time_average+l1_l3_delta){
+		 		miss++;						             	                } else {
+												                        hit ++;
+			}
+			access_time = measure_one_block_access_time((uint64_t)(receiver_buffer+8*(i+1)));
+                        if(access_time > hit_time_average+l1_l3_delta){
+				miss++;
+		        } else {
+                                hit ++;
+                        }
+			access_time = measure_one_block_access_time((uint64_t)(receiver_buffer+8*(i+2)));
+                        if(access_time > hit_time_average+l1_l3_delta){
+                                miss++;
+                        } else {
+                                hit ++;
+                        }
+		}
+		float miss_ratio;
+	        miss_ratio = (float)miss/((float)hit + (float)miss);
+		//printf("miss = %d\t, hit = %d\t, miss_ratio = %f\n", miss, hit, miss_ratio);
+		if(miss_ratio > 0.26){
+			printf("Received '1'. miss = %d, hit = %d, miss_ratio = %f\n", miss, hit, miss_ratio);
+		}		
+		//printf("Receiver finished.\n");
 	}
-
-	printf("Receiver finished.\n");
-
 	return 0;
 }
-
-
--- LAB1/Part2-DeadDrop/sender.c
+++ LAB1/Part2-DeadDrop/sender.c
@@ -27,15 +27,41 @@ int main(int argc, char **argv)
   // TODO:
   // Put your covert channel setup code here
 
+  //Create a write buffer of size L2 that should be written when input is 1.
+  int f = 1.5;
+  uint64_t *sender_buffer = (uint64_t *)malloc(f*32768*sizeof(uint64_t));
+  if (NULL == sender_buffer){
+	  perror("Unable to malloc sender buffer");
+	  return EXIT_FAILURE;
+  }
+
+  volatile char tmp;
+  int reps = 1000;
+
   printf("Please type a message.\n");
 
   bool sending = true;
   while (sending) {
       char text_buf[128];
+      
       fgets(text_buf, sizeof(text_buf), stdin);
 
       // TODO:
       // Put your covert channel code here
+      //printf("input: %s", text_buf);
+      
+      //If input is 1, load write_buffer into cache
+      if(*text_buf == '1'){
+        	for(int j=0; j<4096; j++){
+	  		tmp = sender_buffer[j*8];
+     		}
+      printf("Sent '1'\n");
+      }
+      //char *binary;
+      //binary = string_to_binary(&text_buf);
+      //printf("input-in-binary: %s\n", binary);
+      
+      //sending = false;
   }
 
   printf("Sender finished.\n");
